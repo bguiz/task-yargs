1bb3aa29af6626d22b987ef8adebb3b65702ea4c
Brendan Graetz <github@bguiz.com>
2015-02-26 21:05:23 +1100

    =BG= improve tests
    - reorganise into two files & make describe strings more succinct
    - add validation tests
    - expect().toThrow() instead of doing so manually

--- test/helper.js
+++ test/helper.js
@@ -1,5 +1,58 @@
 'use strict';
 
-module.exports = {
+function checkMustHaveFooUnlessHelp(argv) {
+  if (argv.help) {
+    return true;
+  }
+  else if (argv.foo === 'illegalValue') {
+    throw new Error('Illegal value for foo');
+  }
+  else if (typeof argv.foo === 'undefined') {
+    throw new Error('Missing argument for foo')
+  }
+  else {
+    return true;
+  }
+}
 
+var optionFoo = {
+  key: 'foo',
+  value: {
+    describe: 'A description of the foo option',
+    alias: ['f'],
+    default: 'meh',
+    string: true,
+  },
+};
+
+function checkMustHaveJojoUnlessHelp(argv) {
+  if (argv.help) {
+    return true;
+  }
+  else if (argv.jojo == 'illegalValue') {
+    throw new Error('Illegal value for jojo');
+  }
+  else if (typeof argv.jojo === 'undefined') {
+    throw new Error('Missing argument for jojo');
+  }
+  else {
+    return true;
+  }
+}
+
+var optionJojo = {
+  key: 'jojo',
+  value: {
+    describe: 'A description of the jojo option',
+    alias: ['j', 'jo'],
+    // default is *not* specified
+    string: true,
+  },
+};
+
+module.exports = {
+  checkMustHaveFooUnlessHelp: checkMustHaveFooUnlessHelp,
+  optionFoo: optionFoo,
+  checkMustHaveJojoUnlessHelp: checkMustHaveJojoUnlessHelp,
+  optionJojo: optionJojo,
 };
--- test/main.spec.js
+++ test/main.spec.js
@@ -2,297 +2,198 @@
 
 var yargs = require('yargs');
 var taskYargs = require('../index');
-var helper = require('./helper');
 
-describe('Register a task without dependencies', function() {
-  it('Should register a blank task', function(done) {
-    var taskYargsInstance = taskYargs();
-    taskYargsInstance.register('blank', {
-      description: 'A blank task',
-      prerequisiteTasks: [],
-      checks: [],
-      options: [],
-    });
-    var yargsInstance = taskYargsInstance.get('blank', ['myprocess', 'blank']);
-    var argv = yargsInstance.argv;
-    expect(argv._).toEqual(['blank']);
-    done();
-  });
-
-  it('Should NOT allow any tasks to be registered after first task has been retrieved', function(done) {
-    var taskYargsInstance = taskYargs();
-    taskYargsInstance.register('blank', {
-      description: 'A blank task',
-      prerequisiteTasks: [],
-      checks: [],
-      options: [],
-    });
-    var yargsInstance = taskYargsInstance.get('blank', ['myprocess', 'blank']);
-    var exception;
-    try {
-      taskYargsInstance.register('blank2', {
+describe('[register without prerequisites]', function() {
+  describe('[basic]', function() {
+    it('Should register simple', function(done) {
+      var taskYargsInstance = taskYargs();
+      taskYargsInstance.register('blank', {
         description: 'A blank task',
         prerequisiteTasks: [],
         checks: [],
         options: [],
       });
-    }
-    catch (ex) {
-      exception = ex;
-    }
-    finally {
-      expect(exception && exception.message).toEqual('Not allowed to register new tasks after first task retrieval');
+      var yargsInstance = taskYargsInstance.get('blank', ['blank']);
+      var argv = yargsInstance.argv;
+      expect(argv._).toEqual(['blank']);
       done();
-    }
+    });
+
+    it('Should NOT allow any tasks to be registered after first task has been retrieved', function(done) {
+      var taskYargsInstance = taskYargs();
+
+      taskYargsInstance.register('blank', {
+        description: 'A blank task',
+        prerequisiteTasks: [],
+        checks: [],
+        options: [],
+      });
+      var yargsInstance = taskYargsInstance.get('blank', ['blank']);
+      var exception;
+      var yargsInstance;
+
+      expect(function() {
+        taskYargsInstance.register('blank2', {
+          description: 'A blank task',
+          prerequisiteTasks: [],
+          checks: [],
+          options: [],
+        });
+      }).toThrow('Not allowed to register new tasks after first task retrieval');
+      done();
+    });
+
+    it('Should not allow two tasks with the same name to be registered', function(done) {
+      var taskYargsInstance = taskYargs();
+
+      expect(function() {
+          taskYargsInstance.register('blank', {
+          description: 'A blank task',
+          prerequisiteTasks: [],
+          checks: [],
+          options: [],
+        });
+      }).not.toThrow();
+
+      expect(function() {
+        taskYargsInstance.register('blank', {
+          description: 'A task attempting to overwrite an existing task',
+          prerequisiteTasks: [],
+          checks: [],
+          options: [],
+        });
+      }).toThrow('A task has already been registered with the name blank');
+
+      done();
+    });
   });
 
-  //TODO more validation and error checking related tests
-});
-
-describe('Register task with prerequisite tasks', function() {
-  it('Should resolve prerequisite tasks', function(done) {
+  describe('[validations]', function() {
     var taskYargsInstance = taskYargs();
-    taskYargsInstance.register('child', {
-      description: 'A blank task',
-      prerequisiteTasks: [],
-      checks: [],
-      options: [],
+    it('Should reject registrations without params', function(done) {
+      expect(function() {
+        taskYargsInstance.register(undefined, {});
+      }).toThrow('Must specify a task name');
+      expect(function() {
+        taskYargsInstance.register('', {});
+      }).toThrow('Must specify a task name');
+      expect(function() {
+        taskYargsInstance.register('blank', undefined);
+      }).toThrow('Must specify task object');
+      expect(function() {
+        taskYargsInstance.register('blank', 'notAnObject');
+      }).toThrow('Must specify task object');
+      done();
     });
-    taskYargsInstance.register('father', {
-      description: 'A blank task',
-      prerequisiteTasks: ['child'],
-      checks: [],
-      options: [],
-    });
-    var fatherYargsDefinition = taskYargsInstance.getDefinition('father', ['myprocess', 'father']);
-    expect(fatherYargsDefinition.resolvedPrerequisiteTasks).toEqual(['child']);
-    done();
-  });
-  it('Should resolve prerequisite tasks - multiple levels', function(done) {
-    var taskYargsInstance = taskYargs();
-    taskYargsInstance.register('child', {
-      description: 'A blank task',
-      prerequisiteTasks: [],
-      checks: [],
-      options: [],
-    });
-    taskYargsInstance.register('father', {
-      description: 'A blank task',
-      prerequisiteTasks: ['child'],
-      checks: [],
-      options: [],
-    });
-    taskYargsInstance.register('grandfather', {
-      description: 'A blank task',
-      prerequisiteTasks: ['father'],
-      checks: [],
-      options: [],
-    });
-    var grandFatherYargsDefinition = taskYargsInstance.getDefinition('grandfather', ['myprocess', 'grandfather']);
-    expect(grandFatherYargsDefinition.resolvedPrerequisiteTasks).toEqual(['father', 'child']);
-    var fatherYargsDefinition = taskYargsInstance.getDefinition('father', ['myprocess', 'father']);
-    expect(fatherYargsDefinition.resolvedPrerequisiteTasks).toEqual(['child']);
-    done();
-  });
-  it('Should resolve prerequisite tasks - multiple siblings', function(done) {
-    var taskYargsInstance = taskYargs();
-    taskYargsInstance.register('child', {
-      description: 'A blank task',
-      prerequisiteTasks: [],
-      checks: [],
-      options: [],
-    });
-    taskYargsInstance.register('sister', {
-      description: 'A blank task',
-      prerequisiteTasks: [],
-      checks: [],
-      options: [],
-    });
-    taskYargsInstance.register('father', {
-      description: 'A blank task',
-      prerequisiteTasks: ['child', 'sister'],
-      checks: [],
-      options: [],
-    });
-    taskYargsInstance.register('mother', {
-      description: 'A blank task',
-      prerequisiteTasks: ['child', 'sister'],
-      checks: [],
-      options: [],
-    });
-    taskYargsInstance.register('grandfather', {
-      description: 'A blank task',
-      prerequisiteTasks: ['father', 'mother'],
-      checks: [],
-      options: [],
-    });
-    var grandFatherYargsDefinition = taskYargsInstance.getDefinition('grandfather', ['myprocess', 'grandfather']);
-    expect(grandFatherYargsDefinition.resolvedPrerequisiteTasks).toEqual(['father', 'child', 'sister', 'mother']);
-    var fatherYargsDefinition = taskYargsInstance.getDefinition('father', ['myprocess', 'father']);
-    expect(fatherYargsDefinition.resolvedPrerequisiteTasks).toEqual(['child', 'sister']);
-    done();
-  });
-});
 
-function checkMustHaveFooUnlessHelp(argv) {
-  if (argv.help) {
-    return;
-  }
-  else if (argv.foo === 'illegalValue') {
-    return 'Illegal value for foo';
-  }
-  else {
-    return true;
-  }
-}
+    it('Should reject badly formed task details', function(done) {
+      expect(function() {
+        taskYargsInstance.register('noDescrioption', {
+          description: undefined,
+          prerequisiteTasks: [],
+          checks: [],
+          options: [],
+        });
+      }).toThrow('Task must specify a description');
+      done();
+      expect(function() {
+        taskYargsInstance.register('noPrerequisites', {
+          description: 'A task without prerequisites',
+          prerequisiteTasks: undefined,
+          checks: [],
+          options: [],
+        });
+      }).toThrow('Task must specify prerequisite tasks list');
+      done();
+      expect(function() {
+        taskYargsInstance.register('malformedPrerequisites', {
+          description: 'A task with malformed prerequisites',
+          prerequisiteTasks: ['ok', 123],
+          checks: [],
+          options: [],
+        });
+      }).toThrow('Prerequisite task #1 is not a string');
+      done();
+      expect(function() {
+        taskYargsInstance.register('noChecks', {
+          description: 'A task without checks',
+          prerequisiteTasks: [],
+          checks: undefined,
+          options: [],
+        });
+      }).toThrow('Task must specify checks list');
+      done();
+      expect(function() {
+        taskYargsInstance.register('malformedChecks', {
+          description: 'A task with malformed checks',
+          prerequisiteTasks: [],
+          checks: [function() {}, 123],
+          options: [],
+        });
+      }).toThrow('Check #1 is badly formed');
+      done();
+      expect(function() {
+        taskYargsInstance.register('noOptions', {
+          description: 'A task attempting to overwrite an existing task',
+          prerequisiteTasks: [],
+          checks: [],
+          options: undefined,
+        });
+      }).toThrow('Task must specify options list');
+      done();
+      expect(function() {
+        taskYargsInstance.register('malformedOptions', {
+          description: 'A task attempting to overwrite an existing task',
+          prerequisiteTasks: [],
+          checks: [],
+          options: [{ key: 'flag', value: {} }, 123],
+        });
+      }).toThrow('Option #1 is badly formed');
+      done();
+    });
+  });
 
-var optionFoo = {
-  key: 'foo',
-  value: {
-    describe: 'A description of the foo option',
-    alias: ['f'],
-    default: 'meh',
-    string: true,
-  },
-};
-
-describe('Register task', function() {
-  it('Should register a task with a check and option', function() {
+  describe('[subcommand]', function() {
     var taskYargsInstance = taskYargs();
     taskYargsInstance.register('blank', {
       description: 'A blank task',
       prerequisiteTasks: [],
-      checks: [checkMustHaveFooUnlessHelp],
-      options: [optionFoo],
+      checks: [],
+      options: [],
     });
-    var fooYargsInstance = taskYargsInstance.get('blank', ['myprocess', 'blank', '--foo', 'bar']);
-    expect(fooYargsInstance.argv.foo).toEqual('bar');
-    //test alias too
-    expect(fooYargsInstance.argv.f).toEqual('bar');
-  });
-  it('Should register a task with a check and option with default', function() {
-    var taskYargsInstance = taskYargs();
-    taskYargsInstance.register('blank', {
-      description: 'A blank task',
-      prerequisiteTasks: [],
-      checks: [checkMustHaveFooUnlessHelp],
-      options: [optionFoo],
+    var exception;
+    var yargsInstance;
+
+    it('Should ensure exactly one', function(done) {
+      expect(function() {
+        yargsInstance = taskYargsInstance.get('blank', []);
+        yargsInstance.exitProcess(false);
+        yargsInstance.argv;
+      }).toThrow('Error: No task defined');
+
+      expect(function() {
+        yargsInstance = taskYargsInstance.get('blank', ['blank', 'differentTask']);
+        yargsInstance.exitProcess(false);
+        yargsInstance.argv;
+      }).toThrow('Error: More than one task defined');
+
+      done();
     });
-    var fooYargsInstance = taskYargsInstance.get('blank', ['myprocess', 'blank']);
-    expect(fooYargsInstance.argv.foo).toEqual('meh');
-    //test alias too
-    expect(fooYargsInstance.argv.f).toEqual('meh');
+
+    it('Should ensure matches', function(done) {
+      expect(function() {
+        yargsInstance = taskYargsInstance.get('blank', ['differentTask']);
+        yargsInstance.exitProcess(false);
+        yargsInstance.argv;
+      }).toThrow('Error: Wrong task invoked: differentTask instead of blank');
+
+      expect(function() {
+        yargsInstance = taskYargsInstance.get('blank', ['blank']);
+        yargsInstance.exitProcess(false);
+        yargsInstance.argv;
+      }).not.toThrow();
+
+      done();
+    });
   });
 });
-
-
-describe('Register prerequisite task', function() {
-  it('Should inherit check and option from prerequisite', function() {
-    var taskYargsInstance = taskYargs();
-    var taskYargsInstance = taskYargs();
-    taskYargsInstance.register('child', {
-      description: 'A blank task',
-      prerequisiteTasks: [],
-      checks: [checkMustHaveFooUnlessHelp],
-      options: [optionFoo],
-    });
-    taskYargsInstance.register('father', {
-      description: 'A blank task',
-      prerequisiteTasks: ['child'],
-      checks: [],
-      options: [],
-    });
-    var fatherYargsInstance = taskYargsInstance.get('father', ['myprocess', 'father', '--foo', 'bar']);
-    expect(fatherYargsInstance.argv.foo).toEqual('bar');
-    //test alias too
-    expect(fatherYargsInstance.argv.f).toEqual('bar');
-  });
-  it('Should inherit check and option from prerequisite - with default', function() {
-    var taskYargsInstance = taskYargs();
-    var taskYargsInstance = taskYargs();
-    taskYargsInstance.register('child', {
-      description: 'A blank task',
-      prerequisiteTasks: [],
-      checks: [checkMustHaveFooUnlessHelp],
-      options: [optionFoo],
-    });
-    taskYargsInstance.register('father', {
-      description: 'A blank task',
-      prerequisiteTasks: ['child'],
-      checks: [],
-      options: [],
-    });
-    var fatherYargsInstance = taskYargsInstance.get('father', ['myprocess', 'father']);
-    expect(fatherYargsInstance.argv.foo).toEqual('meh');
-    //test alias too
-    expect(fatherYargsInstance.argv.f).toEqual('meh');
-  });
-  it('Should inherit check and option from prerequisite - multiple levels', function() {
-    var taskYargsInstance = taskYargs();
-    var taskYargsInstance = taskYargs();
-    taskYargsInstance.register('child', {
-      description: 'A blank task',
-      prerequisiteTasks: [],
-      checks: [checkMustHaveFooUnlessHelp],
-      options: [optionFoo],
-    });
-    taskYargsInstance.register('father', {
-      description: 'A blank task',
-      prerequisiteTasks: ['child'],
-      checks: [],
-      options: [],
-    });
-    taskYargsInstance.register('grandfather', {
-      description: 'A blank task',
-      prerequisiteTasks: ['father'],
-      checks: [],
-      options: [],
-    });
-    var grandFatherYargsInstance = taskYargsInstance.get('grandfather', ['myprocess', 'father']);
-    expect(grandFatherYargsInstance.argv.foo).toEqual('meh');
-    //test alias too
-    expect(grandFatherYargsInstance.argv.f).toEqual('meh');
-  });
-  it('Should inherit check and option from prerequisite - multiple siblings', function() {
-    var taskYargsInstance = taskYargs();
-    taskYargsInstance.register('child', {
-      description: 'A blank task',
-      prerequisiteTasks: [],
-      checks: [checkMustHaveFooUnlessHelp],
-      options: [optionFoo],
-    });
-    taskYargsInstance.register('sister', {
-      description: 'A blank task',
-      prerequisiteTasks: [],
-      checks: [],
-      options: [],
-    });
-    taskYargsInstance.register('father', {
-      description: 'A blank task',
-      prerequisiteTasks: ['child', 'sister'],
-      checks: [],
-      options: [],
-    });
-    taskYargsInstance.register('mother', {
-      description: 'A blank task',
-      prerequisiteTasks: ['child', 'sister'],
-      checks: [],
-      options: [],
-    });
-    taskYargsInstance.register('grandfather', {
-      description: 'A blank task',
-      prerequisiteTasks: ['father', 'mother'],
-      checks: [],
-      options: [],
-    });
-    var grandFatherYargsInstance = taskYargsInstance.get('grandfather', ['myprocess', 'father']);
-    expect(grandFatherYargsInstance.argv.foo).toEqual('meh');
-    //test alias too
-    expect(grandFatherYargsInstance.argv.f).toEqual('meh');
-    var fatherYargsInstance = taskYargsInstance.get('father', ['myprocess', 'father', '-f', 'bar']);
-    expect(fatherYargsInstance.argv.foo).toEqual('bar');
-    //test alias too
-    expect(fatherYargsInstance.argv.f).toEqual('bar');
-  });
-});
-
