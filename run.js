0d412e4966a55d9c2953c85237af452c629ca931
Brendan Graetz <github@bguiz.com>
2015-03-05 22:02:36 +1100

    =BG= onRun and onInit in task definitions
    - taskYargsRun wraps arounf taskYargs
    - exposes taskYargs itself plus runCurrent and runByName, but not initialise
    - when a task is run, it automatically
      - runs the onInit function of all its prerequisite tasks plus itself
      - runs the onRun function of itself

---
+++ run.js
@@ -0,0 +1,66 @@
+'use strict';
+
+var taskYargs = require('./index');
+
+function taskYargsRun() {
+  var tyInstance = taskYargs();
+
+
+  function initialise(taskName) {
+    // Call onInit of all prerequisite tasks which them
+    var taskDefinition = tyInstance.getDefinition(taskName);
+    var taskAndPrereqs = [].concat(taskDefinition.resolvedPrerequisiteTasks).concat(taskName);
+    taskAndPrereqs.map(function(name) {
+      return tyInstance.getDefinition(name).onInit;
+    }).filter(function(onInit) {
+      return (typeof onInit === 'function');
+    }).forEach(function(onInit) {
+        onInit();
+    });
+  }
+
+  function runByName(taskName, onRun, processArgv) {
+    processArgv = processArgv || process.argv;
+
+    if (typeof onRun !== 'function') {
+      // If a default onRun function is provided,
+      // it will supersede the one defined in the task defintion
+      var taskDefinition = tyInstance.getDefinition(taskName);
+      onRun = taskDefinition.onRun;
+    }
+
+    if (typeof onRun === 'function') {
+      var yargsInstance = tyInstance.get(taskName, processArgv.slice(2));
+      initialise(taskName);
+      onRun(yargsInstance);
+    }
+    else {
+      throw new Error('Task definition for "' + taskName +
+        '" does not define an onRun function, and no default was provided');
+    }
+  }
+
+  function runCurrent(onRun, processArgv) {
+    processArgv = processArgv || process.argv;
+    var cliArgs;
+    var taskName = tyInstance.getCurrentName(processArgv);
+    if (taskName) {
+      var yargsInstance = tyInstance.get(taskName, processArgv.slice(2));
+      cliArgs = yargsInstance.argv;
+      if (cliArgs.help) {
+        yargsInstance.showHelp();
+      }
+      else {
+        runByName(taskName, onRun, processArgv);
+      }
+    }
+  }
+
+  return {
+    taskYargs: tyInstance,
+    byName: runByName,
+    current: runCurrent,
+  };
+}
+
+module.exports = taskYargsRun;
